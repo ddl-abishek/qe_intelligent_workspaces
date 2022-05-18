0609cc78666569c6f157aa31f83738778564077c
iw Tester6 <rfqa+006@dominodatalab.com>
2022-05-18 16:45:32 -0700

    Pushed from Domino: https://hd-iw-az16310.az.domino.tech/workspace/iw-test6/qe_intelligent_workspaces?executionId=62857a011372130ff29c9809

--- preprocess.py
+++ preprocess.py
@@ -23,23 +23,13 @@ def preprocess_csv(csv_year):
                              usecols=['Vehicle Expiration Date', 'Violation Precinct', 'Issuer Precinct', 'Vehicle Year'], 
                              dtype=dtype).dropna()
     
-    clean_data['Vehicle Expiration Date'] = clean_data['Vehicle Expiration Date'].apply(convert_date, meta=('Vehicle Expiration Date', 
-                                                                                                            dtype['Vehicle Expiration Date']))
+    clean_data['Vehicle Expiration Date'] = clean_data['Vehicle Expiration Date'].apply(convert_date)
     
     if not(os.path.isdir(config['clean_data']['dir'])):
         os.makedirs(config['clean_data']['dir'])
 
-    clean_data.compute().to_csv(config['clean_data'][csv_year], index=False)
+    clean_data.to_csv(config['clean_data'][csv_year], index=False)
 
 if __name__ == "__main__":
-    service_port = os.environ['DASK_SCHEDULER_SERVICE_PORT']
-    service_host = os.environ['DASK_SCHEDULER_SERVICE_HOST']
-
-    client = Client(address=f'{service_host}:{service_port}', direct_to_workers=True)
-    client.wait_for_workers(n_workers=2)
-    client.restart()
-    client.upload_file('utils.py')
-    
-    with performance_report(filename=f"{config['artifacts']['path']}/dask-report_preprocess_{str(datetime.now())}.html"):
-        dask_map = client.map(preprocess_csv, ['2013-14', '2015', '2016', '2017'])
-        client.gather(dask_map)
+    with Pool(6) as p:
+        p.map(preprocess_csv, ['2013-14','2015','2016','2017'])
